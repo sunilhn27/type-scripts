88539cf2158b27f99bdb201d8a947ec06e72483d
Sunil H N <sunilh.n27@gmail.com>
2023-12-08 00:49:04 +0530

    added arrow function and map

--- dest/Functions/function1.js
+++ dest/Functions/function1.js
@@ -8,6 +8,11 @@ function add(a, b) {
     let sum = a + b;
     return sum;
 }
+//funciton with return type
+function upper(value) {
+    let str = value.toUpperCase();
+    return str;
+}
 function isEven(a) {
     if (a % 2 == 0) {
         return true;
@@ -16,6 +21,18 @@ function isEven(a) {
         return false;
     }
 }
+//arrow function
+const addition = () => {
+    return 4;
+};
+const arr = ["sunil", "Raju", "Kushal"];
+function getnames(arr) {
+    return arr.map((element) => {
+        return element.length;
+    });
+}
 console.log(logError("Hi"));
 console.log(add(1, 3));
 console.log(isEven(61));
+console.log(upper("sunil"));
+console.log(getnames(arr)); //[5,4,6]
--- src/Functions/function1.ts
+++ src/Functions/function1.ts
@@ -9,16 +9,39 @@ function add(a: number, b: number): number {
     return sum;
 }
 
+
+//funciton with return type
+function upper(value: string): string {
+    let str: string =value.toUpperCase();
+    return str;
+}
+
 function isEven(a: number): boolean {
     if (a % 2 == 0) {
         return true;
     } else {
         return false;
     }
-
 }
+
+//arrow function
+const addition:()=> number =()=>{
+    return 4;
+}
+
+const arr:Array<string> =["sunil","Raju" ,"Kushal"];
+        
+function getnames(arr:Array<string>):number[]{
+    return arr.map((element) => {
+        return element.length;
+    });
+    
+}
+
 
 console.log(logError("Hi"))
 console.log(add(1, 3))
 console.log(isEven(61));
+console.log(upper("sunil"))
 
+console.log(getnames(arr)); //[5,4,6]
