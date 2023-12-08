00a3960abc55cd8bce6676d1fa19e1f23b49f3f1
Sunil H N <sunilh.n27@gmail.com>
2023-12-09 00:14:05 +0530

    updated datatypes

--- src/DataTypes/DataTypess.ts
+++ src/DataTypes/DataTypess.ts
@@ -4,6 +4,18 @@ const male: boolean = true;
 const numArr: number[] = [1, 2, 3, 4, 5];
 const strArr: string[] = ["Sunil", "Kushal", "Rakesh", "Raju"];
 const bolArr: boolean[] = [true, false, false, true];
+let bin = 0b100;
+let anotherBin: number = 0B010;
+
+
+let lastname:string
+lastname ="sunil";
+
+let items = [0, 1, null, 'Hi']; //(number | string | null)[]
+
+let arr1 = [new Date(), new RegExp('\d+')]; //(RegExp | Date)[]
+
+
 
 console.log(firstName);
 console.log(age);
@@ -12,5 +24,6 @@ console.log(numArr);
 console.log(numArr);
 console.log(strArr);
 console.log(bolArr);
+console.log(lastname.concat("hi"))
 
 
