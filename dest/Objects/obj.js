e41f7fb6a75b2b9543d120da221798c9472208a2
Sunil H N <sunilh.n27@gmail.com>
2023-12-09 00:13:35 +0530

    added Objects

---
+++ dest/Objects/obj.js
@@ -0,0 +1,21 @@
+"use strict";
+const person = { name: "John", age: 30 };
+let employee = {
+    name: "sunil",
+    age: 29,
+    married: false
+};
+let employee1 = {
+    name: "sunil",
+    age: 29,
+    married: false
+};
+let car;
+car = {
+    model: "VM",
+    year: 2013,
+    color: "white"
+};
+console.log(employee1);
+console.log(person);
+console.log(car);
---
+++ src/Objects/obj.ts
@@ -0,0 +1,29 @@
+const person:{name: string,age: number} = { name: "John", age: 30 };
+
+
+let employee: object = {
+    name: "sunil",
+    age: 29,
+    married: false
+}
+
+let employee1: Object = {   //provide some build in addditional methods
+    name: "sunil",
+    age: 29,
+    married: false
+}
+
+let car :{};
+
+car={
+    model:"VM",
+    year: 2013,
+    color: "white"
+}
+
+console.log(employee1);
+console.log(person);
+console.log(car);
+
+
+
