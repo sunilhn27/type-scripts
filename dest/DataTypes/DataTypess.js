4e02416279e3c694a7e90b419e34be311e1f9572
Sunil H N <sunilh.n27@gmail.com>
2023-12-09 00:14:47 +0530

    added dist

--- dest/DataTypes/DataTypess.js
+++ dest/DataTypes/DataTypess.js
@@ -5,6 +5,12 @@ const male = true;
 const numArr = [1, 2, 3, 4, 5];
 const strArr = ["Sunil", "Kushal", "Rakesh", "Raju"];
 const bolArr = [true, false, false, true];
+let bin = 0b100;
+let anotherBin = 0B010;
+let lastname;
+lastname = "sunil";
+let items = [0, 1, null, 'Hi']; //(number | string | null)[]
+let arr1 = [new Date(), new RegExp('\d+')]; //(RegExp | Date)[]
 console.log(firstName);
 console.log(age);
 console.log(male);
@@ -12,3 +18,4 @@ console.log(numArr);
 console.log(numArr);
 console.log(strArr);
 console.log(bolArr);
+console.log(lastname.concat("hi"));
--- dest/Functions/function1.js
+++ dest/Functions/function1.js
@@ -3,6 +3,14 @@
 function logError(msg) {
     return msg;
 }
+//funciton without return type
+function logError1(msg) {
+    console.log("Just log");
+}
+//funciton without return type
+function logError2(msg) {
+    throw new Error("Error me");
+}
 //funciton with return type
 function add(a, b) {
     let sum = a + b;
@@ -31,8 +39,16 @@ function getnames(arr) {
         return element.length;
     });
 }
+let greeting = function () {
+    console.log('Hello');
+};
+function createUser(name, age, gender = "male") {
+    return { name, age, gender };
+}
+console.log(createUser("sunil", 29)); //{ name: 'sunil', age: 29, gender: 'male' }
 console.log(logError("Hi"));
 console.log(add(1, 3));
 console.log(isEven(61));
 console.log(upper("sunil"));
 console.log(getnames(arr)); //[5,4,6]
+console.log('Hello'.length); // 5
