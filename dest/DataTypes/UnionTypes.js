17a56f09b73144543ebcdb3015a3e8079f481f8a
Sunil H N <sunilh.n27@gmail.com>
2023-12-12 00:11:41 +0530

    added union types

---
+++ dest/DataTypes/UnionTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+function union(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    else {
+        throw new Error("paramtre not matching");
+    }
+}
+function add(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(union("sunil", "hn"));
+console.log(add(2, 5));
---
+++ src/DataTypes/UnionTypes.ts
@@ -0,0 +1,26 @@
+function union(a:any, b:any){
+   if(typeof a=== 'number' && typeof b==='number'){
+      return a+b;
+   }if (typeof a==='string' && typeof b==='string'){
+    return a.concat(b);
+   }else{
+    throw new Error("paramtre not matching");
+   }
+
+}
+
+
+
+function add(a: number | string, b: number | string) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+
+
+console.log(union("sunil","hn"))
+console.log(add(2,5));
