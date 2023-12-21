c458dfedbdce12b61db22b72cbd7be811c61febb
Sunil H N <sunilh.n27@gmail.com>
2023-12-22 00:09:40 +0530

    added Generics

--- dest/Generics/MyGenerics.js
+++ dest/Generics/MyGenerics.js
@@ -5,3 +5,15 @@ function identity(arg) {
 // Usage
 let res = identity(123); // T is inferred as number
 let value = identity("abc"); // T is inferred as string
+function newGenerics(first, sec) {
+    return { first, sec };
+}
+let n = newGenerics(5, "sunil");
+let b = newGenerics(true, new Date());
+console.log(n);
+console.log(b);
+function newGenerics1(first, sec) {
+    return sec;
+}
+let k = newGenerics1(5, "sunil");
+console.log(k);
--- src/Generics/MyGenerics.ts
+++ src/Generics/MyGenerics.ts
@@ -5,3 +5,23 @@ function identity<T>(arg: T): T {
 // Usage
 let res: number = identity(123);   // T is inferred as number
 let value: string = identity("abc");  // T is inferred as string
+
+
+function newGenerics<T, U>(first: T, sec: U): { first: T, sec: U } {
+    return { first, sec };
+}
+
+
+let n = newGenerics<number, string>(5, "sunil");
+let b = newGenerics<boolean, Date>(true, new Date());
+
+
+console.log(n)
+console.log(b)
+
+function newGenerics1<T, U>(first: T, sec: U): U {
+    return sec;
+}
+
+let k = newGenerics1<number, string>(5, "sunil");
+console.log(k)
